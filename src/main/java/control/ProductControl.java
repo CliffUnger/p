f31d36394b87acf8bc2f51114e59fe40d190ca4c
CliffUnger <adrianobuccino@gmail.com>
2024-05-23 15:05:13 +0200

    id-3 unresolved

--- src/main/java/control/ProductControl.java
+++ src/main/java/control/ProductControl.java
@@ -21,6 +21,7 @@ import model.ProductModel;
 /**
  * Servlet implementation class ProductControl
  */
+
 public class ProductControl extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
